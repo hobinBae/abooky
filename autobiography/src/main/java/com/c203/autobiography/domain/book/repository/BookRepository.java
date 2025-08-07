6d760ee5872eacf87d3f07fd349c31fadc3b0791
배호빈 <qoghqls95@naver.com>
2025-08-07 14:52:30 +0900

    feat: 책 좋아요 기능 추가

--- autobiography/src/main/java/com/c203/autobiography/domain/book/repository/BookRepository.java
+++ autobiography/src/main/java/com/c203/autobiography/domain/book/repository/BookRepository.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
 
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
     Optional<Book> findByBookIdAndDeletedAtIsNull(Long bookId);
